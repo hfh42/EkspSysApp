2860fa834a9396c1bd129bab1323eca25512d5c2
hfh42 <hfh9042@gmail.com>
2013-05-13 16:13:01 +0200

    Created 3 init activities

--- src/net/haagh/ekspsysapp/FunActivity.java
+++ src/net/haagh/ekspsysapp/FunActivity.java
@@ -1,33 +1,34 @@
 package net.haagh.ekspsysapp;
 
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class FunActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_fun);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.fun, menu);
 		return true;
 	}
-	
+
+	public void gotoHome(View view){
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);		
+	}
+
 	public void gotoCalender(View view){
 		Intent intent = new Intent(this, CalenderActivity.class);
 		startActivity(intent);		
 	}
 
-	public void gotoFun(View view){
-		Intent intent = new Intent(this, FunActivity.class);
-		startActivity(intent);		
-	}
 }

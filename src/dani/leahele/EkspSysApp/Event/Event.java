bb898fa07b9feb1fcdeb1d05f4f5f68d3a4e9352
hfh42 <hfh9042@gmail.com>
2013-05-14 11:19:39 +0200

    Added Event list i calender

--- src/dani/leahele/EkspSysApp/Event/Event.java
+++ src/dani/leahele/EkspSysApp/Event/Event.java
@@ -1,20 +1,25 @@
 package dani.leahele.EkspSysApp.Event;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Event {
+public class Event implements Serializable{
+	
+	private static final long serialVersionUID = -5613769324766155412L;
 	
 	public final String title;
 	public final String date;
+	public final String time;
 	public final String singupDate;
 	public final int price;
 	public final String info;
 	
 	private ArrayList<String> registered = new ArrayList<String>();
 	
-	public Event(String title, String date, String signupDate, int price, String info){
+	public Event(String title, String date, String time, String signupDate, int price, String info){
 		this.title = title;
 		this.date = date;
+		this.time = time;
 		this.singupDate = signupDate;
 		this.price = price;
 		this.info = info;
--- src/dani/leahele/EkspSysApp/FunActivity.java
+++ src/dani/leahele/EkspSysApp/FunActivity.java
@@ -1,5 +1,6 @@
 package dani.leahele.EkspSysApp;
 
+import dani.leahele.EkspSysApp.Calender.CalenderActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;

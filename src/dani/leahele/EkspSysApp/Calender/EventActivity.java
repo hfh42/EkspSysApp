230baf83aa6c4386e38aa18ac93a1eab75994fbc
hfh42 <hfh9042@gmail.com>
2013-05-14 11:49:41 +0200

    Created event layout, and start on event activity

--- src/dani/leahele/EkspSysApp/Calender/EventActivity.java
+++ src/dani/leahele/EkspSysApp/Calender/EventActivity.java
@@ -1,18 +1,32 @@
 package dani.leahele.EkspSysApp.Calender;
 
-import dani.leahele.EkspSysApp.R;
-import dani.leahele.EkspSysApp.R.layout;
-import dani.leahele.EkspSysApp.R.menu;
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import dani.leahele.EkspSysApp.FunActivity;
+import dani.leahele.EkspSysApp.MainActivity;
+import dani.leahele.EkspSysApp.R;
+import dani.leahele.EkspSysApp.Event.Event;
 
 public class EventActivity extends Activity {
+	private Event event;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event);
+		
+		Intent intent = getIntent();
+		event = (Event) intent.getSerializableExtra("dani.leahele.EkspSysApp.event");
+		
+		TextView title = (TextView) findViewById(R.id.event_title);
+		TextView date = (TextView) findViewById(R.id.event_date);
+		TextView time = (TextView) findViewById(R.id.event_time);
+		TextView signup = (TextView) findViewById(R.id.event_signup);
+		TextView info = (TextView) findViewById(R.id.event_info);
 	}
 
 	@Override
@@ -20,6 +34,21 @@ public class EventActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.event, menu);
 		return true;
+	}
+
+	public void gotoHome(View view) {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+
+	public void gotoCalender(View view) {
+		Intent intent = new Intent(this, CalenderActivity.class);
+		startActivity(intent);
+	}
+
+	public void gotoFun(View view) {
+		Intent intent = new Intent(this, FunActivity.class);
+		startActivity(intent);
 	}
 
 }
